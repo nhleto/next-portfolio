58a90ccf2113aa3823e6e8c8bb06fbe222686890
henry <hnr.leto11@gmail.com>
2022-10-29 12:23:35 -0400

    update css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,31 @@
+import styles from "./Header.module.scss";
+
 export default function Header() {
+  const gitHubUrl = "https://github.com/nhleto";
+
   return (
-    <header>
-      <nav class-name="navbar">
-        <div class-name="start-nav">
-          <div class-name="nav-info">
-            <h3 class-name="nav-name">Henry Leto</h3>
-            <div class-name="nav-description">Full Stack Developer</div>
+    <header className={styles.navbar}>
+        <div className={styles.startNav}>
+          <div>
+            <h3 className={styles.navName}>Henry Leto</h3>
+            <div className={styles.navDescription}>Full Stack Developer</div>
           </div>
         </div>
-        <div class-name="end-nav">
-          {/* <h3 class-name="nav-item" onclick="about()"> */}
+        <div className={styles.endNav}>
+          {/* <h3 className={styles.navItem} onclick="about()"> */}
           {/* TODO: Add back in ONCLICK functions */}
-          <h3 class-name="nav-item">About</h3>
-          <h3 class-name="nav-item">Projects</h3>
-          <h3 class-name="nav-item">Contact</h3>
+          <h3 className={styles.navItem}>About</h3>
+          <h3 className={styles.navItem}>Projects</h3>
+          <h3 className={styles.navItem}>Contact</h3>
           <a
-            href="https://github.com/nhleto"
+            href={gitHubUrl}
             target="_blank"
             rel="noopener noreferrer"
-            class-name="nav-item"
+            className={styles.navItem}
           >
             Github
           </a>
         </div>
-      </nav>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
+import styles from "../styles/globals.scss"
 
 const Home: NextPage = () => {
-  return <Header />;
+  return (
+    <Header />
+  );
 };
 
 export default Home;
